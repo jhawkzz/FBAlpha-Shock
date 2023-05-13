2e3f0034d3fbcb1b53f0c8eabe9bfe00037108ef
Trapper McFerron <trapper.mcferron@gmail.com>
2023-05-13 10:44:29 -0700

    input works

--- src/burner/shock/core/windows/input.h
+++ src/burner/shock/core/windows/input.h
@@ -4,43 +4,39 @@
 #ifndef INPUT_H_
 #define INPUT_H_
 
-// Keyboard device
-#define KB_LOCAL_DEVICE     "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
-#define INPUT_POLL_TIMEOUT  (300)
-
 enum InputCodeToButtonMapping
 {
-    P1_Start     = 2, // Keyboard 1
-    OptionsBack  = 6, // Keyboard 5
-    SelectGame   = 7, // Keyboard 6
-    P2_Start     = 3, // Keyboard 2
-    
-    P2_Yellow    = 21, // Keyboard Y
-    P2_Green     = 22, // Keyboard U
-    P2_Blue      = 23, // Keyboard I
-    
-    P1_Yellow    = 30, // Keyboard A
-    P1_Green     = 31, // Keyboard S
-    P1_Blue      = 32, // Keyboard D
-    
-    P2_Red       = 35, // Keyboard H
-    P2_MidBot    = 36, // Keyboard J
-    P2_RightBot  = 37, // Keyboard K
-    
-    P1_Red       = 44, // Keyboard Z
-    P1_MidBot    = 45, // Keyboard X
-    P1_RightBot  = 46, // Keyboard C
-    
-    P1_Joy_Up    = 103, // Keyboard Up Arrow
-    P2_Joy_Up    = 102, // Keyboard Home
-    P1_Joy_Right = 106, // Keyboard Right Arrow
-    P2_Joy_Down  = 107, // Keyboard End
-    P1_Joy_Down  = 108, // Keyboard Down Arrow
-    P1_Joy_Left  = 105, // Keyboard Left Arrow
-    P2_Joy_Right = 109, // Keyboard PgDn
-    P2_Joy_Left  = 111, // Keyboard Delete
-    
-    Button_Count
+    P1_Start,
+    OptionsBack,
+    SelectGame,
+    P2_Start,
+
+    P2_Yellow,
+    P2_Green,
+    P2_Blue,
+
+    P1_Yellow,
+    P1_Green,
+    P1_Blue,
+
+    P2_Red,
+    P2_MidBot,
+    P2_RightBot,
+
+    P1_Red,
+    P1_MidBot,
+    P1_RightBot,
+
+    P1_Joy_Up,
+    P2_Joy_Up ,
+    P1_Joy_Right,
+    P2_Joy_Down,
+    P1_Joy_Down,
+    P1_Joy_Left,
+    P2_Joy_Right,
+    P2_Joy_Left,
+
+    Button_Count = 24
 };
 
 struct ButtonState
