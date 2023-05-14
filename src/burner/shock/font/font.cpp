51a9abda9940250f23ddd610e4eb9578640d4768
jhawkzz <jeredmcferron@gmail.com>
2023-05-13 18:57:44 -0700

    for real

--- src/burner/shock/font/font.cpp
+++ src/burner/shock/font/font.cpp
@@ -11,7 +11,7 @@
 char Font::mFont[ FONT_LETTER_COUNT ] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C',
                                           'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
                                           'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ',', '.', '!',
-                                          '%', ':', '#', '-', '/', '\\', '(', ')', '*', '&', '@', '`' }; //# = Copyright logo
+                                          '%', ':', '#', '-', '/', '\\', '(', ')', '*', '&', '@', '\'' }; //# = Copyright logo
 
 char       Font::mCharLookup[ MAX_CHAR_LOOKUP ];
 FontObject Font::mFontObject[ FontType_Count ];
