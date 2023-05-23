3edbeaf6c98486fc2e0b315192b2d1b5606a1daa
Trapper McFerron <trapper.mcferron@gmail.com>
2023-05-22 22:05:06 -0700

    wip

--- src/burner/shock/core/input.h
+++ src/burner/shock/core/input.h
@@ -4,41 +4,6 @@
 #ifndef INPUT_H_
 #define INPUT_H_
 
-enum InputCodeToButtonMapping
-{
-    P1_Start,
-    OptionsBack,
-    SelectGame,
-    P2_Start,
-
-    P2_Yellow,
-    P2_Green,
-    P2_Blue,
-
-    P1_Yellow,
-    P1_Green,
-    P1_Blue,
-
-    P2_Red,
-    P2_MidBot,
-    P2_RightBot,
-
-    P1_Red,
-    P1_MidBot,
-    P1_RightBot,
-
-    P1_Joy_Up,
-    P2_Joy_Up ,
-    P1_Joy_Right,
-    P2_Joy_Down,
-    P1_Joy_Down,
-    P1_Joy_Left,
-    P2_Joy_Right,
-    P2_Joy_Left,
-
-    Button_Count
-};
-
 #include "shock/platform/input.h"
 
 class Input : public InputImpl
