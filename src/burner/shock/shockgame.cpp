c7dac2a9aecf02389557cf0a0c69f689451bebcc
jhawkzz <jeredmcferron@gmail.com>
2023-05-26 15:28:19 -0700

    save states anyone?

--- src/burner/shock/shockgame.cpp
+++ src/burner/shock/shockgame.cpp
@@ -484,6 +484,11 @@ void ShockGame::UpdateResetMode( )
     }
 }
 
+void ShockGame::SaveState( )
+{
+    BurnStateSave( ShockRomLoader::GetRomsetName(), 1 );
+}
+
 void ShockGame::ResetFBATickTime( )
 {
     // reset our game tick timing. These are used to monitor drift
