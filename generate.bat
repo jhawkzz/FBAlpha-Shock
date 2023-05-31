e083ec38ef9d9db6783f7977f7a5a60bbd9ecab5
Trapper McFerron <trapper.mcferron@gmail.com>
2023-05-30 17:45:28 -0700

    wip

--- generate.bat
+++ generate.bat
@@ -3,5 +3,5 @@ premake5 --file=./premake/premake5_lubuntu.lua gmake2
 premake5 --file=./premake/premake5_mvsx.lua gmake2
 premake5 --file=./premake/premake5_asp.lua gmake2
 premake5 --file=./premake/premake5_lubuntu.lua codelite
-rem premake5 --file=./premake/premake5_mvsx.lua codelite
-rem premake5 --file=./premake/premake5_asp.lua codelite
+premake5 --file=./premake/premake5_mvsx.lua codelite
+premake5 --file=./premake/premake5_asp.lua codelite
