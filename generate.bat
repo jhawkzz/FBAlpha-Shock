61091081c6edef778e7c976ba63dbbbdbad0f34a
mcferront <trapper.mcferron@gmail.com>
2023-06-16 22:00:07 -0700

    Remove Projects directory (#20)

--- generate.bat
+++ generate.bat
@@ -8,3 +8,4 @@ rem premake5 --file=./premake/premake5_lubuntu.lua codelite
 rem premake5 --file=./premake/premake5_mvsx_asp.lua codelite
 
 copy .\premake\make.bat_ .\projects\mvsx_asp\make.bat
+copy .\premake\linker-fix.txt .\projects\mvsx_asp\linker-fix.txt
